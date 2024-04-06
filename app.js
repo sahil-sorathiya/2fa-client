58669708a4637464be1ed9fd00a7cedacc4453e4
sahil-sorathiya <sahil.patel@gmail.com>
2024-04-07 00:08:24 +0530

    minor change

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const { isLoggedIn } = require('./middlewares/isLoggedIn');
 const app = express()
 const port = 3000
 
+app.set('views', './views');
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
