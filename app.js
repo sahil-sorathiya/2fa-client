f76426af67097c91275c31bb3ea1e19aee038152
sahil-sorathiya <sahil.patel@gmail.com>
2024-04-07 00:17:26 +0530

    minor change

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const port = 3000
 
 app.set('views', path.resolve(path.join(__dirname, '/views')));
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(path.resolve(path.join(__dirname, '/public'))))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cookieParser())
